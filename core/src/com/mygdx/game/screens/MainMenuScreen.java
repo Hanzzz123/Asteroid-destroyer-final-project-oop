920bb9eb447f52bf66515742e94b910684745231
algazahp <algazahp19@gmail.com>
2023-11-25 01:28:47 +0700

    second

--- core/src/com/mygdx/game/screens/MainMenuScreen.java
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -3,103 +3,110 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.towerDefenceGame;
 
 public class MainMenuScreen implements Screen {
 
-    private static final int NEW_GAME_BUTTON_WIDTH= 380;
-    private static final int NEW_GAME_BUTTON_HEIGHT = 177;
+    private static final int BUTTON_WIDTH = 380;
+    private static final int BUTTON_HEIGHT = 100;
 
-    private static final int LOAD_GAME_BUTTON_WIDTH= 410;
-    private static final int LOAD_GAME_BUTTON_HEIGHT = 171;
+    private towerDefenceGame game;
+    private Stage stage;
 
-    private static final int SETTINGS_BUTTON_WIDTH= 309;
-    private static final int SETTINGS_BUTTON_HEIGHT = 158;
-
-    private static final int EXIT_BUTTON_WIDTH= 330;
-    private static final int EXIT_BUTTON_HEIGHT = 222;
-
-    private static final int NEW_GAME_BUTTON_Y = 240;
-    private static final int LOAD_GAME_BUTTON_Y = 180;
-    private static final int SETTINGS_GAME_BUTTON_Y = 120;
-    private static final int EXIT_GAME_BUTTON_Y = 10;
-    towerDefenceGame game;
-
-    Texture newGameButtonActive;
-    Texture newGameButtonInactive;
-    Texture loadGameButtonActive;
-    Texture loadGameButtonInactive;
-    Texture settingsButtonActive;
-    Texture settingButtonInactive;
-    Texture exitButtonActive;
-    Texture exitButtonInactive;
-
-    Table table;
-    public MainMenuScreen(towerDefenceGame game){
+    public MainMenuScreen(towerDefenceGame game) {
         this.game = game;
-        newGameButtonActive = new Texture("newGameButtonActive.png");
-        loadGameButtonActive = new Texture("loadGameButtonActive.png");
-        settingsButtonActive = new Texture("settingsButtonActive.png");
-        exitButtonActive = new Texture("exitButtonActive.png");
+        this.stage = new Stage(new ScreenViewport());
 
+        setupUI();
+    }
+
+    private void setupUI() {
+        Table table = new Table();
+        table.setFillParent(true);
+
+        TextButton newGameButton = new TextButton("", game.skin, "newGameButtonActive");
+        TextButton loadGameButton = new TextButton("", game.skin, "loadGameButtonActive");
+        TextButton settingsButton = new TextButton("", game.skin, "settingsButtonActive");
+        TextButton exitButton = new TextButton("", game.skin, "exitButtonActive");
+
+        table.add(newGameButton).width(BUTTON_WIDTH).height(BUTTON_HEIGHT).padBottom(20).row();
+        table.add(loadGameButton).width(BUTTON_WIDTH).height(BUTTON_HEIGHT).padBottom(20).row();
+        table.add(settingsButton).width(BUTTON_WIDTH).height(BUTTON_HEIGHT).padBottom(20).row();
+        table.add(exitButton).width(BUTTON_WIDTH).height(BUTTON_HEIGHT).padBottom(20).row();
+
+        stage.addActor(table);
+
+        newGameButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Handle New Game button click
+            }
+        });
+
+        loadGameButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Handle Load Game button click
+            }
+        });
+
+        settingsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Handle Settings button click
+            }
+        });
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Handle Exit button click
+                Gdx.app.exit(); // For desktop applications
+            }
+        });
     }
 
     @Override
     public void show() {
-
+        
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
 
-
-        int x = towerDefenceGame.WIDTH / 2 - NEW_GAME_BUTTON_WIDTH /2;
-        game.batch.draw(newGameButtonActive,x, NEW_GAME_BUTTON_Y, EXIT_BUTTON_WIDTH,EXIT_BUTTON_HEIGHT);
-
-        x = towerDefenceGame.WIDTH / 2 - LOAD_GAME_BUTTON_WIDTH /2;
-        game.batch.draw(loadGameButtonActive,x, LOAD_GAME_BUTTON_Y,LOAD_GAME_BUTTON_WIDTH,LOAD_GAME_BUTTON_HEIGHT);
-
-        x = towerDefenceGame.WIDTH / 2 - SETTINGS_BUTTON_WIDTH / 2;
-        game.batch.draw(settingsButtonActive,x, SETTINGS_GAME_BUTTON_Y,SETTINGS_BUTTON_WIDTH,SETTINGS_BUTTON_HEIGHT);
-
-        x = towerDefenceGame.WIDTH / 2 - EXIT_BUTTON_WIDTH / 2;
-        game.batch.draw(exitButtonActive,x,EXIT_GAME_BUTTON_Y,EXIT_BUTTON_WIDTH,EXIT_BUTTON_HEIGHT);
-
-        game.batch.end();
-
-
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
+
+    // Other methods...
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
--- core/src/com/mygdx/game/towerDefenceGame.java
+++ core/src/com/mygdx/game/towerDefenceGame.java
@@ -1,21 +1,82 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.mygdx.game.screens.MainMenuScreen;
 
 public class towerDefenceGame extends Game {
-	public static final int WIDTH = 1440;
-	public static final int HEIGHT = 720;
-	public SpriteBatch batch;
+    public static final int WIDTH = 1440;
+    public static final int HEIGHT = 720;
+    public SpriteBatch batch;
+    public Skin skin;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		this.setScreen(new MainMenuScreen(this));
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        skin = new Skin(); // Create a default skin
 
-	public void render() {
-		super.render();
-	}
+        // Add your button textures to the skin
+        skin.add("newGameButtonActive", new Texture("newGameButtonActive.png"));
+        skin.add("loadGameButtonActive", new Texture("loadGameButtonActive.png"));
+        skin.add("settingsButtonActive", new Texture("settingsButtonActive.png"));
+        skin.add("exitButtonActive", new Texture("exitButtonActive.png"));
+
+        // Create a default BitmapFont
+        BitmapFont font = new BitmapFont();
+        skin.add("default-font", font);
+
+        // Create a TextButton style for newGameButtonActive
+        TextButton.TextButtonStyle newGameButtonStyle = new TextButton.TextButtonStyle();
+        newGameButtonStyle.up = skin.getDrawable("newGameButtonActive");
+        newGameButtonStyle.down = skin.getDrawable("loadGameButtonActive");
+        newGameButtonStyle.checked = skin.getDrawable("settingsButtonActive");
+        newGameButtonStyle.over = skin.getDrawable("exitButtonActive");
+        newGameButtonStyle.font = skin.getFont("default-font");
+
+        // Register the TextButtonStyle for newGameButtonActive
+        skin.add("newGameButtonActive", newGameButtonStyle);
+
+        // Create a TextButton style for loadGameButtonActive
+        TextButton.TextButtonStyle loadGameButtonStyle = new TextButton.TextButtonStyle();
+        loadGameButtonStyle.up = skin.getDrawable("loadGameButtonActive");
+        loadGameButtonStyle.down = skin.getDrawable("loadGameButtonActive");
+        loadGameButtonStyle.checked = skin.getDrawable("loadGameButtonActive");
+        loadGameButtonStyle.over = skin.getDrawable("loadGameButtonActive");
+        loadGameButtonStyle.font = skin.getFont("default-font");
+
+        // Register the TextButtonStyle for loadGameButtonActive
+        skin.add("loadGameButtonActive", loadGameButtonStyle);
+
+        // Create a TextButton style for settingsButtonActive
+        TextButton.TextButtonStyle settingsButtonStyle = new TextButton.TextButtonStyle();
+        settingsButtonStyle.up = skin.getDrawable("settingsButtonActive");
+        settingsButtonStyle.down = skin.getDrawable("settingsButtonActive");
+        settingsButtonStyle.checked = skin.getDrawable("settingsButtonActive");
+        settingsButtonStyle.over = skin.getDrawable("settingsButtonActive");
+        settingsButtonStyle.font = skin.getFont("default-font");
+
+        // Register the TextButtonStyle for settingsButtonActive
+        skin.add("settingsButtonActive", settingsButtonStyle);
+
+        // Create a TextButton style for exitButtonActive
+        TextButton.TextButtonStyle exitButtonStyle = new TextButton.TextButtonStyle();
+        exitButtonStyle.up = skin.getDrawable("exitButtonActive");
+        exitButtonStyle.down = skin.getDrawable("exitButtonActive");
+        exitButtonStyle.checked = skin.getDrawable("exitButtonActive");
+        exitButtonStyle.over = skin.getDrawable("exitButtonActive");
+        exitButtonStyle.font = skin.getFont("default-font");
+
+        // Register the TextButtonStyle for exitButtonActive
+        skin.add("exitButtonActive", exitButtonStyle);
+
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render();
+    }
 }
