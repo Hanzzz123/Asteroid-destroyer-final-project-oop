d90b5c2add2762b9cec2d54db97ee8c69735364a
Hanzzz123 <aanzs496@gmail.com>
2023-11-29 23:57:34 +0700

    add tiled map

---
+++ assets/maps/PC Computer - Shadow Warrior - Animated Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="PC Computer - Shadow Warrior - Animated Tiles" tilewidth="72" tileheight="72" tilecount="180" columns="9">
+ <image source="PC Computer - Shadow Warrior - Animated Tiles.png" width="684" height="1444"/>
+</tileset>
